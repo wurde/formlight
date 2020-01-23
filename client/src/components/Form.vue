5aba958730ef8e153706f439d5e20540eed8e5af
Andy Bettisworth <andy@accreu.com>
2020-01-23 17:46:29 -0600

    Style links

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <h3>Form</h3>
+    <a v-on:click.prevent="toggleCurrentView">See submissions</a>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Form',
-  props: ['isEditing']
+  props: ['isEditing'],
+  methods: {
+    toggleCurrentView: function() {
+      alert('toggle')
+    }
+  }
 }
 </script>
 
 <style scoped>
+a {
+  text-decoration: underline;
+  cursor: pointer;
+}
 </style>
--- client/src/components/Submissions.vue
+++ client/src/components/Submissions.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <h3>Submissions</h3>
+    <a v-on:click.prevent="toggleCurrentView">See form</a>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Submissions',
-  props: ['isEditing']
+  props: ['isEditing'],
+  methods: {
+    toggleCurrentView: function() {
+      alert('toggle')
+    }
+  }
 }
 </script>
 
 <style scoped>
+a {
+  text-decoration: underline;
+  cursor: pointer;
+}
 </style>
