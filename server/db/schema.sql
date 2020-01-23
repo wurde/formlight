55587c14671a108f76dc878d59a84432eca44b22
Andy Bettisworth <andy@accreu.com>
2020-01-22 20:32:00 -0600

    Design submissions table in SQL

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -6,4 +6,11 @@ CREATE TABLE IF NOT EXISTS forms (
   fields_json text
 );
 
+CREATE TABLE IF NOT EXISTS submissions (
+  id integer PRIMARY KEY AUTOINCREMENT,
+  form_title text NOT NULL,
+  fields_json text,
+  answers_json text
+);
+
 END TRANSACTION;
