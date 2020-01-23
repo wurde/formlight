310d50b937a0400aea9db67484406aa5a8c7a0af
Andy Bettisworth <andy@accreu.com>
2020-01-22 19:24:17 -0600

    Remove HelloWorld component

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,18 +2,15 @@
   <div id="app">
     <Toolbar />
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Toolbar from './components/Toolbar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     Toolbar
   }
 }
