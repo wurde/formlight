a079a0d580457f1eb415fa4ba351eed2397da686
Andy Bettisworth <andy@accreu.com>
2020-01-22 22:11:57 -0600

    Verify all endpoints exist

--- server/tests/forms_router.spec.js
+++ server/tests/forms_router.spec.js
@@ -19,4 +19,24 @@ describe('forms_router.js', () => {
   it("GET /forms", () => {
     return request(app).get('/forms').expect(200)
   })
+
+  it("POST /forms", () => {
+    return request(app).post('/forms').expect(200)
+  })
+
+  it("GET /forms/:id", () => {
+    return request(app).get('/forms/1').expect(200)
+  })
+
+  it("PUT /forms/:id", () => {
+    return request(app).put('/forms/1').expect(200)
+  })
+
+  it("PATCH /forms/:id", () => {
+    return request(app).patch('/forms/1').expect(200)
+  })
+
+  it("DELETE /forms/:id", () => {
+    return request(app).delete('/forms/1').expect(200)
+  })
 })
--- server/tests/submissions_router.spec.js
+++ server/tests/submissions_router.spec.js
@@ -16,7 +16,37 @@ describe('submissions_router.js', () => {
     expect(process.env.NODE_ENV).toBe('test');
   });
 
-  it("GET /forms/1/submissions", () => {
+  it("GET /forms/:form_id/submissions", () => {
     return request(app).get('/forms/1/submissions').expect(200)
   })
+
+  it("POST /forms/:form_id/submissions", () => {
+    return request(app)
+      .post("/forms/1/submissions")
+      .expect(200);
+  });
+
+  it("GET /forms/:form_id/submissions/:id", () => {
+    return request(app)
+      .get("/forms/1/submissions/1")
+      .expect(200);
+  });
+
+  it("PUT /forms/:form_id/submissions/:id", () => {
+    return request(app)
+      .put("/forms/1/submissions/1")
+      .expect(200);
+  });
+
+  it("PATCH /forms/:form_id/submissions/:id", () => {
+    return request(app)
+      .patch("/forms/1/submissions/1")
+      .expect(200);
+  });
+
+  it("DELETE /forms/:form_id/submissions/:id", () => {
+    return request(app)
+      .delete("/forms/1/submissions/1")
+      .expect(200);
+  });
 })
