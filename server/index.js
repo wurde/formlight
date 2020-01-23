f0cfb6bd777b8338bbdbcaaac064d755b92ee9f8
Andy Bettisworth <andy@accreu.com>
2020-01-22 19:45:18 -0600

    Initi express server

---
+++ server/index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const express = require('express');
+
+/**
+ * Constants
+ */
+
+const port = process.env.PORT || 3000;
+
+/**
+ * Define app
+ */
+
+const app = express();
+
+/**
+ * Mount routes
+ */
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+})
+
+/**
+ * Start server
+ */
+
+if (module === require.main) {
+  app.listen(port, () => {
+    console.log(`Express server running on port ${port}`);
+  })
+}
