02148e18970799a6080ae9ec023aec771417e4f2
Andy Bettisworth <andy@accreu.com>
2020-01-23 17:35:15 -0600

    Define and render Main component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header v-bind:isEditing="isEditing" v-on:toggleIsEditing="toggleIsEditing" />
+    <Main v-bind:isEditing="isEditing" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Main
   },
   data: function() {
     return {
---
+++ client/src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Main</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: ['isEditing']
+}
+</script>
+
+<style scoped>
+</style>
