bbcade7d52058992ef3e97bd4bfe14bc0d0ceab3
Andy Bettisworth <andy@accreu.com>
2020-01-22 20:34:32 -0600

    Forms migration

--- server/db/migrations/20200122202857_create_forms.js
+++ server/db/migrations/20200122202857_create_forms.js
@@ -1,8 +1,13 @@
+'use strict'
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('forms', t => {
+    t.increments();
+    t.text('title').notNullabl();
+    t.text('fields_json');
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('forms');
 };
