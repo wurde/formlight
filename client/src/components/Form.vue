0c05a49dec751258e879bf433b8f1cc463938c37
Andy Bettisworth <andy@accreu.com>
2020-01-24 15:36:28 -0600

    Listen for and handle errors

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -2,7 +2,10 @@
   <section class="form-section">
     <div v-show="isEditing" class="editable">
       <h3>Form</h3>
-      <form v-on:submit.prevent="saveForm">
+
+      <div v-show="error" class="errors">{{error}}</div>
+
+      <form @submit.prevent="submit">
         <label for="form-title">Title</label>
         <input id="form-title" type="text" name="title" v-model="title" autofocus />
 
@@ -23,20 +26,30 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+const backendURL = 'http://localhost:3000';
+
 export default {
   name: 'Form',
   props: ['isEditing'],
   data: function() {
     return {
-      title: 'ExampleForm'
+      title: 'ExampleForm',
+      error: null
     }
   },
   methods: {
     toggleCurrentView: function() {
       alert('toggle')
     },
-    saveForm: function() {
-      alert('save')
+    submit: function() {
+      axios.post(backendURL + '/forms')
+      .then(res => alert(JSON.stringify(res)))
+      .catch(err => {
+        const res = err.response;
+        this.error = `Error: ${res.data.error.message}`;
+      })
     }
   }
 }
@@ -74,5 +87,11 @@ a {
 
 a:hover {
   text-shadow: 0 5px 30px #EEC480;
+}
+
+.errors {
+  color: red;
+  font-weight: bold;
+  padding-bottom: 10px;
 }
 </style>
