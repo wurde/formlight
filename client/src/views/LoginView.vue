ef08f2c7b21def2d4d9a15709365dd5dbc20fda7
Andy Bettisworth <andy@accreu.com>
2020-02-05 11:30:13 -0600

    Style Login page

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="login">
-    <h3>Login</h3>
-    <form @submit="loginForm" method="post">
-      <label for="input-username">Username</label>
-      <input id="input-username" type="text" v-model="username" autofocus />
-      <button type="submit">Enter</button>
-    </form>
-  </div>
+  <main id="login">
+    <div class="container justify-content-center text-align-center">
+      <h1>Login</h1>
+
+      <form @submit="loginForm" method="post">
+        <div class="input-group">
+          <input id="input-username" type="text" v-model="username" placeholder="Username" autofocus />
+        </div>
+        <button type="submit">Enter</button>
+      </form>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -26,3 +30,89 @@ export default {
   }
 }
 </script>
+
+<style>
+main {
+  margin: 0 auto;
+}
+
+h1 {
+  display: flex;
+}
+
+label {
+  display: inline-block;
+  font-weight: bold;
+}
+
+input {
+  width: 100%;
+  height: 2em;
+  padding: 15px;
+  margin: 20px 0px;
+  border-radius: 5px;
+  border: 1px solid #CBDBEA;
+  font-size: 18px;
+  outline: none;
+}
+
+input:focus {
+  box-shadow: 0 3px 15px #CBDBEA;
+}
+
+button {
+  width: 100%;
+  height: 3em;
+  background-color: #FFF;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 24px;
+  border: none;
+  border: 1px solid #7FB8A4;
+  color: #7FB8A4;
+  outline: none;
+}
+button:focus, button:hover {
+  background-color: #7FB8A4;
+  color: #FFF;
+}
+
+form {
+  background-color: #FDFDFE;
+  border: 1px solid #DDD;
+  padding: 25px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+}
+.justify-content-center {
+  justify-content: center;
+}
+.text-align-center {
+  text-align: center;
+}
+.input-group {
+  display: block;
+  width: 100%;
+  magrin-bottom: 1.2rem;
+}
+
+@media (min-width: 576px) {
+  main { width: 90%; }
+}
+
+@media (min-width: 768px) {
+  main { width: 80%; }
+}
+
+@media (min-width: 992px) {
+  main { width: 60%; }
+}
+
+@media (min-width: 1200px) {
+  main { width: 40%; }
+}
+</style>
